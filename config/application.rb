2f48fe369905629cc12103dc6cab0abbb7093d9a
yuhkiikd <yuhki.ikd@gmail.com>
2019-11-16 05:14:09 +0000

    001_first_commit

---
+++ config/application.rb
@@ -0,0 +1,21 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+Bundler.require(*Rails.groups)
+
+module OwntaskApp
+  class Application < Rails::Application
+    config.load_defaults 5.2
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.jbuilder false
+    end
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+  end
+end
