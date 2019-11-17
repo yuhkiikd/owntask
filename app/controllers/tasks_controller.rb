08870f04ab21ed48bb7f142f709a2db4f8ced637
yuhkiikd <yuhki.ikd@gmail.com>
2019-11-17 09:31:12 +0000

    create_crud_to_task

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,65 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all.order(id: "DESC")
+  end
+
+  def new
+    if params[:back]
+      @task = Task.new(task_params)
+    else
+      @task = Task.new
+    end
+  end
+
+  def confirm
+    @task = Task.new(task_params)
+    render :new if @task.invalid?
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if params[:back]
+      render :new
+    else
+      if @task.save
+        flash[:success] = 'タスクを作成しました！'
+        redirect_to tasks_path
+      else
+        render :new
+      end
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      flash[:success] = 'タスクを編集しました！'
+      redirect_to tasks_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    flash[:success] = 'タスクを削除しました！'
+    redirect_to tasks_url
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :priority, :status, :limit)
+  end
+end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,7 @@
 class Task < ApplicationRecord
+  validates :title, presence: true
+  validates :details, presence: true
+  validates :priority, presence: true
+  validates :status, presence: true
+  validates :limit, presence: true
 end
