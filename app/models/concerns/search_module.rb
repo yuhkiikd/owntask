672f21795816a10af15fb05aecc40118aa1d9b13
yuhkiikd <yuhki.ikd@gmail.com>
2019-11-25 16:30:44 +0000

    022_サーチ機能モジュール化

--- app/models/concerns/search_module.rb
+++ app/models/concerns/search_module.rb
@@ -8,5 +8,25 @@ module SearchModule
     scope :sort_title, -> (title){where('title LIKE ?', "%#{ title }%")}
     scope :sort_status, -> (status){where('status LIKE ?', "%#{ status }%")}
     scope :sort_title_and_status, -> (title,status){sort_title(title).sort_status(status)}
+
+    def self.set_sort(params)
+      if params[:sort_limit_desc]
+        desc_limit
+      elsif params[:asc_priority]
+        asc_priority
+      elsif params[:task].present?
+        if params[:task][:title].present? && params[:task][:status].present?
+          sort_title_and_status(params[:task][:title],params[:task][:status])
+        elsif params[:task][:status].present?
+          sort_status(params[:task][:status])
+        elsif params[:task][:title].present?
+          sort_title(params[:task][:title])
+        else
+          desc_sort_create_at
+        end
+      else
+        desc_sort_create_at
+      end
+    end
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -7,23 +7,4 @@ class Task < ApplicationRecord
   validates :status, presence: true
   validates :limit,  presence: true
 
-  def self.set_sort(params)
-    if params[:sort_limit_desc]
-      desc_limit
-    elsif params[:asc_priority]
-      asc_priority
-    elsif params[:task].present?
-      if params[:task][:title].present? && params[:task][:status].present?
-        sort_title_and_status(params[:task][:title],params[:task][:status])
-      elsif params[:task][:status].present?
-        sort_status(params[:task][:status])
-      elsif params[:task][:title].present?
-        sort_title(params[:task][:title])
-      else
-        desc_sort_create_at
-      end
-    else
-      desc_sort_create_at
-    end
-  end
 end
