45d475e4ff13759ad5ddbac47e059a7d8699f5b6
yuhkiikd <yuhki.ikd@gmail.com>
2019-12-01 12:11:26 +0000

    038_優先度とステータスが指定文字以外入力出来ないよう修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,7 +3,7 @@ class Task < ApplicationRecord
 
   validates :title, presence: true
   validates :details, presence: true
-  validates :priority, presence: true
-  validates :status, presence: true
+  validates :priority, inclusion: { in: %w(A B C D E) }, presence: true
+  validates :status, inclusion: { in: %w(未着手 着手中 完了) }, presence: true
   validates :limit, presence: true
 end
