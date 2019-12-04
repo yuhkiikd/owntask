33583da3446dfc927d25088b38d6be1978cd5c35
yuhkiikd <yuhki.ikd@gmail.com>
2019-12-04 14:03:07 +0000

    042_ログアウト・セッション機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   http_basic_authenticate_with :name => ENV['USER'], :password => ENV['PASS'] if Rails.env == "production"
+  protect_from_forgery with: :exception
+  include SessionsHelper
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'tasks#index'
   resources :tasks do
     collection do
       get :sort
     end
   end
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users
   resources :labels, only: [:create, :destroy, :show]
 end
