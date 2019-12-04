e285accb770328d21497b58718069914786cc396
yuhkiikd <yuhki.ikd@gmail.com>
2019-12-04 12:29:03 +0000

    040_ユーザーモデルとタスクモデルの紐づけ

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: true
+  before_validation { email.downcase! }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 8 }
+  has_many :tasks
 end
