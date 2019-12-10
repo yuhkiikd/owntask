c0121b6eb61c350e05fa5faaf38a98cc31a621a6
yuhkiikd <yuhki.ikd@gmail.com>
2019-12-10 11:15:37 +0000

    058_seedファイル加筆

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
 if Rails.env == "development"
+  1.times do |i|
+    User.create!(name: "admin", email: "admin@hoge.com", password: "hogehoge", admin: true)
+  end
+
   10.times do |i|
-    User.create!(name: "hoge#{i + 1}", email: "hoge#{i + 1}@hoge.com", password_digest: "hogehoge")
+    User.create!(name: "hoge#{i + 1}", email: "hoge#{i + 1}@hoge.com", password: "hogehoge")
   end
 end 
