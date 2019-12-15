8b8a2e8641ad16294004eaa03b99d6e7c2181964
yuhkiikd <yuhki.ikd@gmail.com>
2019-12-15 16:01:32 +0000

    067_ラベルの表示微調整、リンクの修正

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,6 +1,4 @@
 class Label < ApplicationRecord
-  validates :label_type, presence: true
-
   has_many :labellings, dependent: :destroy, foreign_key: 'label_id'
   has_many :tasks, through: :labellings, source: :task
 
